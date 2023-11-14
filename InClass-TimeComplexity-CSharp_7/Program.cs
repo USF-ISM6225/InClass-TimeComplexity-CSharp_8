3abfe266be06a966a1ab013cab69731f5da20a7f
ISM6225 <ISM6225Class@gmail.com>
2023-11-13 21:20:37 -0500

    Add project files.

---
+++ InClass-TimeComplexity-CSharp_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+int[] data = { 6, 2, 4, 1, 3, 5 };
+for (int j = 0; j < data.Length; j++)
+{
+    int key = data[j];
+    int i = j - 1;
+
+    while ((i >= 0) && (data[i] > key))
+    {
+        data[i + 1] = data[i];
+        i = i - 1;
+    }
+
+    data[i + 1] = key;
+
+    for (int sorted = 0; sorted < data.Length; sorted++)
+    {
+        Console.Write(data[sorted] + " ");
+    }
+    Console.Write("\n");
+}
